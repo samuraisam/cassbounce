a4511b7f69f35b20d56ed27b1327a4ae299c665b
Samuel Sutch <samuraiblog@gmail.com>
2012-08-01 01:15:39 -0700

    test client - does some basic things

---
+++ testclient.py
@@ -0,0 +1,18 @@
+import time
+import pycassa
+
+
+pool = pycassa.ConnectionPool('cassbounce', server_list=['0.0.0.0:9216'])
+# pool = pycassa.ConnectionPool('cassbounce', server_list=['0.0.0.0:9160'])
+pool.fill()
+
+cf = pycassa.ColumnFamily(pool, 'farts')
+
+print list(cf.get_range())
+
+while True:
+	rowkey = raw_input("key: ")
+	column = raw_input("col: ")
+	value = raw_input("value: ")
+
+	cf.insert(rowkey, {column: value})
