3dec494132f67221a2933b73fdf560e2de61d447
Samuel Sutch <samuraiblog@gmail.com>
2012-08-03 02:08:06 -0700

    go fmt

--- cassbounce/main.go
+++ cassbounce/main.go
@@ -9,10 +9,10 @@ func main() {
 	shutdown := make(chan int)
 
 	initial := []server.CassandraHost{server.CassandraHost{"0.0.0.0", 9160}}
-	
+
 	hostList := server.NewCassandraHostList(initial, true, shutdown)
 
 	log.Print("host list ", hostList)
 
 	server.Listen("0.0.0.0:9216")
-}	
+}
